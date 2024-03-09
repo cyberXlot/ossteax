528792480f3f37545f866e2b2032665a2440b20b
cyberXlot gg <ahmadagu08@gmail.com>
2024-03-09 13:46:20 +0000

    oss tea

--- validate.js
+++ validate.js
@@ -2,6 +2,7 @@ const chalk = require ("chalk")
 
 function validateTea() {
     console.log(chalk.red("Henlo Tea"));
+    console.log(chalk.blue("Ready to submit"));
 }
 
 module.exports = validateTea
